3a8ddf1e14bc9b82b3971ea6d011aab6dd9211de
Dhruv <39978989+Dhruv9066@users.noreply.github.com>
2026-09-01 15:58:40 -0600

    saved

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -1,5 +1,5 @@
 module "website" {
-  source = "git::https://github.com/Dhruv9066/terraform-aws-s3-bucket.git?ref=v1.0.1"
+  source = "git::https://github.com/Dhruv9066/terraform-aws-s3-bucket-website.git?ref=v1.0.1"
   bucket_name = var.bucket_name
   tags = var.tag
   region = var.region
